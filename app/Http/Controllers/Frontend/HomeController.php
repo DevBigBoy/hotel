30d8af896d1fc264aaa6d3ea9629307b2c328e56
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-02-20 03:50:55 +0200

    Add Show Book Area in Frontend

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\BookArea;
 use App\Models\Team;
 use Illuminate\Http\Request;
 
@@ -11,10 +12,13 @@ class HomeController extends Controller
     public function index()
     {
         $teams = Team::active()->latest()->get();
-
+        $bookArea = BookArea::latest()->first();
         return view(
             'frontend.index',
-            ['teams' => $teams]
+            [
+                'teams' => $teams,
+                'bookarea' => $bookArea
+            ]
         );
     }
 }
--- resources/views/frontend/home/book.blade.php
+++ resources/views/frontend/home/book.blade.php
@@ -1,27 +1,27 @@
-<div class="book-area-two pt-100 pb-70">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-lg-6">
-                <div class="book-content-two">
-                    <div class="section-title">
-                        <span class="sp-color">MAKE A QUICK BOOKING</span>
-                        <h2>We Are the Best in All-time & So Please Get a Quick Booking</h2>
-                        <p>
-                            Atoli is one of the best resorts in the global market and that's why you will get a
-                            luxury life period on the global market. We always
-                            provide you a special support for all of our guests and that's will be the main reason
-                            to be the most popular.
-                        </p>
+@if ($bookarea)
+    <div class="book-area-two pt-100 pb-70">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="book-content-two">
+                        <div class="section-title">
+                            <span class="sp-color">{{ $bookarea->title }}</span>
+                            <h2>{{ $bookarea->short_title }}</h2>
+                            <p>
+                                {!! $bookarea->description !!}
+                            </p>
+                        </div>
+                        <a href="{{ $bookarea->link }}" class="default-btn btn-bg-three">Quick Booking</a>
                     </div>
-                    <a href="#" class="default-btn btn-bg-three">Quick Booking</a>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="book-img-2">
+                        <img src="{{ asset('storage/' . $bookarea->image) }}" alt="Images">
+                    </div>
                 </div>
             </div>
 
-            <div class="col-lg-6">
-                <div class="book-img-2">
-                    <img src="{{ asset('frontend/assets/img/book-img2.jpg') }}" alt="Images">
-                </div>
-            </div>
         </div>
     </div>
-</div>
+@endif
